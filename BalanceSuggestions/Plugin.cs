0bc75037046b8cae233d51a1ec50868f43ef98d1
HardworkingSlacker <roman.windt@web.de>
2024-08-17 17:50:49 +0200

    Archers Spire Balance Suggestions Added Logic for Balancing several Properties of Archers Spire. Can be viewed in detail in Bepinex Mod Config File.

--- BalanceSuggestions/Plugin.cs
+++ BalanceSuggestions/Plugin.cs
@@ -1,5 +1,10 @@
-﻿using BepInEx;
+﻿using BalanceSuggestions.Utils;
+using BepInEx;
 using BepInEx.Logging;
+using HarmonyLib;
+
+using BalanceSuggestions.Patches;
+using BepInEx.Configuration;
 
 namespace BalanceSuggestions;
 
@@ -7,11 +12,50 @@ namespace BalanceSuggestions;
 public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
-        
+
+    private ConfigEntry<bool> debug;
+    private ConfigEntry<PatchLogger.LoggerMethods> loggerMethod;
+
+    private ConfigEntry<int> newAdditionalArrowsToShoot;
+    private ConfigEntry<float> newRangeMulti;
+    private ConfigEntry<float> newAttackCooldownMulti;
+    private ConfigEntry<float> newProjectileSpeedMulti;
+    private ConfigEntry<float> newSlowDuration;
+
+    private void configureArcherSpirePatches()
+    {
+        ArcherSpire_Patch_TowerUpgrades.newAdditionalArrowsToShoot = newAdditionalArrowsToShoot.Value;
+        ArcherSpire_Patch_TowerUpgrades.newRangeMulti = newRangeMulti.Value;
+        ArcherSpire_Patch_TowerUpgrades.newAttackCooldownMulti = newAttackCooldownMulti.Value;
+        ArcherSpire_Patch_TowerUpgrades.newProjectileSpeedMulti = newProjectileSpeedMulti.Value;
+        ArcherSpire_Patch_TowerUpgrades.newSlowDuration = newSlowDuration.Value;
+    }
+
+    private void loadConfig()
+    {
+        debug = Config.Bind<bool>("General", "Debug", false, "Enables Logging of Debug Messages.");
+        loggerMethod = Config.Bind("General", "LoggerMethod", PatchLogger.LoggerMethods.Bepinex, "What Channel to use for Logging.");
+
+        newAdditionalArrowsToShoot = Config.Bind<int>("Spires.ArchersSpire", "AdditionalArrows", 0, "How many additional Arrows an Archer's Spire grants. (0 to disable)");
+        newRangeMulti = Config.Bind<float>("Spires.ArchersSpire", "RangeMultiplier", 0f, "New Range Multiplier for Archer's Spire. (0 to disable)");
+        newAttackCooldownMulti = Config.Bind<float>("Spires.ArchersSpire", "AttackCooldownMultiplier", 0f, "New Attack Cooldown Multiplier for Archer's Spire. (0 to disable)");
+        newProjectileSpeedMulti = Config.Bind<float>("Spires.ArchersSpire", "ProjectileSpeedMultiplier", 0f, "New Projectile Speed Multiplier for Archer's Spire. (0 to disable)");
+        newSlowDuration = Config.Bind<float>("Spires.ArchersSpire", "AttackSlowdownDuration", 0f, "Slowdown Duration in seconds for Archer's Spire. (0 to disable)");
+    }
+
     private void Awake()
     {
+        loadConfig();
+
+        configureArcherSpirePatches();
+
         // Plugin startup logic
         Logger = base.Logger;
+        PatchLogger.IsDebugEnabled = debug.Value;
+        PatchLogger.BepinexLogger = Logger;
+        PatchLogger.loggerMethod = loggerMethod.Value;
+        Harmony harmony = new Harmony("balsug.patch");
+        harmony.PatchAll();
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
     }
 }
---
+++ BalanceSuggestions/Utils/LogWriter.cs
@@ -0,0 +1,36 @@
+﻿
+using HarmonyLib;
+using BepInEx.Logging;
+
+namespace BalanceSuggestions.Utils
+{
+    internal class PatchLogger
+    {
+        public static bool IsDebugEnabled { get; set; }
+
+        public enum LoggerMethods
+        {
+            File,
+            Bepinex,
+        }
+
+        public static LoggerMethods loggerMethod = LoggerMethods.File;
+
+        public static ManualLogSource BepinexLogger;
+
+        public static void Log(string message)
+        {
+            if (IsDebugEnabled)
+            {
+                switch (loggerMethod){
+                    case LoggerMethods.File:
+                        FileLog.Log(message);
+                        break;
+                    case LoggerMethods.Bepinex:
+                        BepinexLogger.LogDebug(message);
+                        break;
+                }
+            }
+        }
+    }
+}
